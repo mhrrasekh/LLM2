fe22e77ddfe90eec0f3b19afaf655dff419b4da3
mhrrasekh <mhr1379sin@gmail.com>
2026-09-24 08:15:38 +0330

    feat: add provider capabilities

---
+++ src/providers/capabilities.js
@@ -0,0 +1,9 @@
+export const CHATGPT_CAPABILITIES = {
+  chat: true,
+  streaming: "compatibility",
+  systemMessages: true,
+  conversationSessions: true,
+  tools: false,
+  vision: false,
+  files: false
+};
