993ae9d644c42d43cc5400adb21f1a8ded1073c5
mhrrasekh <mhr1379sin@gmail.com>
2026-09-24 08:13:02 +0330

    feat: add request-manager.js

---
+++ src/request-manager.js
@@ -0,0 +1,42 @@
+export class RequestManager {
+  constructor({ maxQueueSize = 4 } = {}) {
+    this.maxQueueSize = maxQueueSize;
+    this.running = false;
+    this.queue = [];
+  }
+
+  get status() {
+    return { running: this.running, queued: this.queue.length };
+  }
+
+  run(task) {
+    if (this.running || this.queue.length) {
+      if (this.queue.length >= this.maxQueueSize) {
+        const error = new Error("REQUEST_QUEUE_FULL");
+        error.code = "REQUEST_QUEUE_FULL";
+        return Promise.reject(error);
+      }
+    }
+
+    return new Promise((resolve, reject) => {
+      this.queue.push({ task, resolve, reject });
+      this.drain();
+    });
+  }
+
+  async drain() {
+    if (this.running) return;
+    const item = this.queue.shift();
+    if (!item) return;
+
+    this.running = true;
+    try {
+      item.resolve(await item.task());
+    } catch (error) {
+      item.reject(error);
+    } finally {
+      this.running = false;
+      queueMicrotask(() => this.drain());
+    }
+  }
+}
